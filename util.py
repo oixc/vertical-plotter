f96b12b8a8e7ee24f1b194d9f55909f6b9a05e54
oixc <jan.h.hofmann@googlemail.com>
2021-04-04 17:58:26 +0200

    utility function

---
+++ util.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  4 17:32:03 2021
+
+@author: oixc
+"""
+
+command_dict = {'LL': 'a',
+                'LR': 'b',
+                'RL': 'c',
+                'RR': 'd',
+                'PU': 'e',
+                'PD': 'f'}
