7290515efc53c29a72449f0cd8523368f5c37bdb
oixc <jan.h.hofmann@googlemail.com>
2021-04-04 19:51:14 +0200

    step sequence which would produce a straight line on an orthogonal grid but not on this polar grid

--- plotter.py
+++ plotter.py
@@ -29,19 +29,41 @@ class Plotter(simulation.Simulation):
         delta_line_lenght = target_line_length - current_line_lenght
         line_steps = delta_line_lenght / self.step_unit
         
+        step_sequence = []
         if any(abs(line_steps) < 0.5):
-            step_sequence = []
+            # only one line needs to change lenght
             for anchor in [0, 1]:
                 direction = np.sign(line_steps[anchor])
                 number_of_steps = int(abs(np.round(line_steps[anchor], 0)))
                 step_sequence.extend(number_of_steps * [self._step_command(anchor, direction)])
             
             return step_sequence
-
-        steps_ratio = line_steps[0] / line_steps[1]
         
+        step_sequence = []
         
+        # inspired by https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+        directions = []
+        number_of_steps = []
+        for anchor in [0, 1]:
+            directions.append(np.sign(line_steps[anchor]))
+            number_of_steps.append(int(abs(np.round(line_steps[anchor], 0))))
 
+        target_step_ratio = number_of_steps[0] / number_of_steps[1]
+        while sum(number_of_steps) > 0:
+            if number_of_steps[1] == 0:
+                anchor = 0
+            else:
+                if number_of_steps[0] / number_of_steps[1] > target_step_ratio :
+                    anchor = 0
+                else:
+                    anchor = 1
+            direction = directions[anchor]
+            step_sequence.append(self._step_command(anchor, direction))
+            number_of_steps[anchor] -= 1
+
+        return step_sequence
+    
+    
 if __name__ == '__main__':    
     send_commands = arduino_serial.send_commands
     
@@ -52,15 +74,14 @@ if __name__ == '__main__':
     #     sim.send_commands(commands)
     #     arduino_serial.send_commands(commands)
 
-
     p = Plotter()
     x, y = 150, 100
     x, y = p.pen_position
-    x = 100
+    x = 10
+    x, y = 200, 100
     step_sequence = p.find_step_squence(x, y)
     send_commands(['f'] + step_sequence + ['e']) 
-    
-    
+        
     # send_commands(['f'] + ['c', 'c', 'a'] * 30 + ['e'] + ['a', 'a', 'd'] * 20 + ['f'] + ['a', 'c'] * 30 + ['e']) 
     
     sim.draw_svg()
