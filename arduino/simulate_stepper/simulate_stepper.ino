bd98cbfb2ee060db38e79453aa092be430a96dc0
oixc <jan.h.hofmann@googlemail.com>
2021-04-04 13:00:49 +0200

    add rotate function and send rotation to serial

--- arduino/simulate_stepper/simulate_stepper.ino
+++ arduino/simulate_stepper/simulate_stepper.ino
@@ -13,10 +13,18 @@ int pins[2][2] = {
 };
 
 void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
   pinMode(pins[MOTOR_LEFT][DIRECTION_LEFT], OUTPUT);
   pinMode(pins[MOTOR_LEFT][DIRECTION_RIGHT], OUTPUT);
   pinMode(pins[MOTOR_RIGHT][DIRECTION_LEFT], OUTPUT);
   pinMode(pins[MOTOR_RIGHT][DIRECTION_RIGHT], OUTPUT);
+
+  Serial.println('A');  // send READY
 }
 
 void blink(int pin) { 
@@ -25,23 +33,36 @@ void blink(int pin) {
   digitalWrite(pin, LOW);     
 }
   
-void rotate(int pin) { 
-  blink(pin); 
+void rotate(int motor, int dir) { 
+  switch (motor) {
+    case MOTOR_LEFT:
+      Serial.print('L');
+      break;
+    case MOTOR_RIGHT:
+      Serial.print('R');
+      break;
+  }
+  switch (dir) {
+    case DIRECTION_LEFT:
+      Serial.println('l');
+      break;
+    case DIRECTION_RIGHT:
+      Serial.println('r');
+      break;
+  }
+  blink(pins[motor][dir]); 
 }
-
-void rotateLeft(int motor) { 
-  rotate(pins[motor][DIRECTION_LEFT]); 
-}
-
-void rotateRight(int motor) { 
-  rotate(pins[motor][DIRECTION_RIGHT]); 
-}
-
 
 void loop() {
-  blink(pins[MOTOR_LEFT][DIRECTION_LEFT]);
-  blink(pins[MOTOR_LEFT][DIRECTION_RIGHT]);
-  blink(pins[MOTOR_RIGHT][DIRECTION_LEFT]);
-  blink(pins[MOTOR_RIGHT][DIRECTION_RIGHT]);
-  delay(2000);                       
+//  blink(pins[MOTOR_LEFT][DIRECTION_LEFT]);
+//  blink(pins[MOTOR_LEFT][DIRECTION_RIGHT]);
+//  blink(pins[MOTOR_RIGHT][DIRECTION_LEFT]);
+//  blink(pins[MOTOR_RIGHT][DIRECTION_RIGHT]);
+//  delay(2000);                       
+ 
+  rotate(MOTOR_LEFT, DIRECTION_LEFT);
+  rotate(MOTOR_LEFT, DIRECTION_RIGHT);    
+  rotate(MOTOR_RIGHT, DIRECTION_LEFT);
+  rotate(MOTOR_RIGHT, DIRECTION_RIGHT);    
+  delay(1000);     
 }
