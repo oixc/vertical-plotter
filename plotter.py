0a4475205f134b8b20f1f6529a04297e646680b6
oixc <jan.h.hofmann@googlemail.com>
2021-04-04 23:56:56 +0200

    straight lines on a polar grid

--- plotter.py
+++ plotter.py
@@ -42,26 +42,74 @@ class Plotter(simulation.Simulation):
         
         step_sequence = []
         
-        # inspired by https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-        directions = []
-        number_of_steps = []
-        for anchor in [0, 1]:
-            directions.append(np.sign(line_steps[anchor]))
-            number_of_steps.append(int(abs(np.round(line_steps[anchor], 0))))
-
-        target_step_ratio = number_of_steps[0] / number_of_steps[1]
-        while sum(number_of_steps) > 0:
-            if number_of_steps[1] == 0:
-                anchor = 0
-            else:
-                if number_of_steps[0] / number_of_steps[1] > target_step_ratio :
+        orthogonal_grid = False
+        if orthogonal_grid:
+            # inspired by https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+            directions = []
+            number_of_steps = []
+            for anchor in [0, 1]:
+                directions.append(np.sign(line_steps[anchor]))
+                number_of_steps.append(int(abs(np.round(line_steps[anchor], 0))))
+    
+            target_step_ratio = number_of_steps[0] / number_of_steps[1]
+            while sum(number_of_steps) > 0:
+                if number_of_steps[1] == 0:
                     anchor = 0
                 else:
-                    anchor = 1
-            direction = directions[anchor]
-            step_sequence.append(self._step_command(anchor, direction))
-            number_of_steps[anchor] -= 1
+                    if number_of_steps[0] / number_of_steps[1] > target_step_ratio :
+                        anchor = 0
+                    else:
+                        anchor = 1
+                direction = directions[anchor]
+                step_sequence.append(self._step_command(anchor, direction))
+                number_of_steps[anchor] -= 1
 
+        else:
+            current_point = np.array(self.pen_position)
+            target_point = np.array([x, y])
+            delta_point = target_point - current_point
+            delta_max_steps = int(abs(np.ceil(np.sum(delta_point / self.step_unit))))
+            
+            left_anchor = np.array(self.anchor_points[0])
+            right_anchor = np.array(self.anchor_points[1])
+            
+            prev_left_steps = -1
+            prev_right_steps = -1
+            left_delta = 0
+            right_delta = 0
+            
+            for i in np.linspace(0, 1, delta_max_steps):
+                temp_point = current_point + i * delta_point
+                left = np.linalg.norm(temp_point - left_anchor)
+                right = np.linalg.norm(temp_point - right_anchor)
+                
+                left_steps = np.round(left / self.step_unit, 0)
+                right_steps = np.round(right / self.step_unit, 0)
+                
+                if prev_left_steps > -1:
+                    left_delta = left_steps - prev_left_steps
+                    right_delta = right_steps - prev_right_steps
+                
+                if abs(left_delta) > 0:
+                    # assert abs(left_delta) < 2
+                    anchor = 0
+                    direction = np.sign(left_delta)
+                    for _ in range(int(abs(left_delta))):
+                        step_sequence.append(self._step_command(anchor, direction))
+                    
+                if abs(right_delta) > 0:
+                    # assert abs(right_delta) < 2
+                    anchor = 1
+                    direction = np.sign(right_delta)
+                    for _ in range(int(abs(right_delta))):
+                        step_sequence.append(self._step_command(anchor, direction))
+                
+                # if abs(right_delta) + abs(left_delta) > 0:    
+                #     print(temp_point, left_steps, right_steps, left_delta, right_delta)
+                
+                prev_left_steps = left_steps
+                prev_right_steps = right_steps
+                
         return step_sequence
     
     def sequence_to(self, x, y, draw_line=False):
@@ -95,68 +143,69 @@ if __name__ == '__main__':
 
     p = Plotter()
     
-    sim.anchor_points[0] = (0, 0)
-    sim.anchor_points[1] = (1750, 0)
-    sim.guesstimate_line_lenth()
-    p.anchor_points = sim.anchor_points.copy()
-    p.line_length = sim.line_length.copy()
-    
-    sim.step_unit = p.step_unit = 1 # 0.1
-    # x, y = 150, 100
-    # x, y = p.pen_position
-    # x = 10
-    # x, y = 200, 100
-    # step_sequence = p.find_step_squence(x, y)
-    # send_commands(['f'] + step_sequence + ['e']) 
+    if True:
+        sim.anchor_points[0] = (0, 0)
+        sim.anchor_points[1] = (1750, 0)
+        sim.guesstimate_line_lenth()
+        p.anchor_points = sim.anchor_points.copy()
+        p.line_length = sim.line_length.copy()
         
-    # send_commands(['f'] + ['c', 'c', 'a'] * 30 + ['e'] + ['a', 'a', 'd'] * 20 + ['f'] + ['a', 'c'] * 30 + ['e']) 
-    
-    intermediate_steps = 1
-    
-    all_commands = []
-    ## rectanlge(100, 100, 100, 100)
-    # all_commands.extend(p.move_to(100, 100))
-    # for i in range(intermediate_steps):
-    #     all_commands.extend(p.line_to(100 + (100 / intermediate_steps) * (i + 1), 100))
-    # for i in range(intermediate_steps):
-    #     all_commands.extend(p.line_to(200, 100 + (100 / intermediate_steps) * (i + 1)))
-    # for i in range(intermediate_steps):
-    #     all_commands.extend(p.line_to(200 - (100 / intermediate_steps) * (i + 1), 200))
-    # for i in range(intermediate_steps):
-    #     all_commands.extend(p.line_to(100, 200 - (100 / intermediate_steps) * (i + 1)))
-    
-    # all_commands.extend(p.line_to(1, 100))
-    # all_commands.extend(p.line_to(150, 100))
-    # all_commands.extend(p.line_to(299, 100))
-    
-    filename = 'test_pattern'
-    with open(f'svg/{filename}.path', 'r') as f:
-        path = f.read()
+        sim.step_unit = p.step_unit = 1 # 0.1
+        # x, y = 150, 100
+        # x, y = p.pen_position
+        # x = 10
+        # x, y = 200, 100
+        # step_sequence = p.find_step_squence(x, y)
+        # send_commands(['f'] + step_sequence + ['e']) 
+            
+        # send_commands(['f'] + ['c', 'c', 'a'] * 30 + ['e'] + ['a', 'a', 'd'] * 20 + ['f'] + ['a', 'c'] * 30 + ['e']) 
         
-    xs = []
-    ys = []
+        intermediate_steps = 1
         
-    path = iter(path.split())
-    for action in path:
-        x = float(next(path)) * 0.4
-        y = float(next(path)) * 0.4
+        all_commands = []
+        ## rectanlge(100, 100, 100, 100)
+        # all_commands.extend(p.move_to(100, 100))
+        # for i in range(intermediate_steps):
+        #     all_commands.extend(p.line_to(100 + (100 / intermediate_steps) * (i + 1), 100))
+        # for i in range(intermediate_steps):
+        #     all_commands.extend(p.line_to(200, 100 + (100 / intermediate_steps) * (i + 1)))
+        # for i in range(intermediate_steps):
+        #     all_commands.extend(p.line_to(200 - (100 / intermediate_steps) * (i + 1), 200))
+        # for i in range(intermediate_steps):
+        #     all_commands.extend(p.line_to(100, 200 - (100 / intermediate_steps) * (i + 1)))
         
-        xs.append(x)
-        ys.append(y)
+        # all_commands.extend(p.line_to(1, 100))
+        # all_commands.extend(p.line_to(150, 100))
+        # all_commands.extend(p.line_to(299, 100))
         
-        if action == 'M':
-            all_commands.extend(p.move_to(x, y))
-        elif action == 'L':
-            all_commands.extend(p.line_to(x, y))
-        else:
-            raise NotImplementedError()
+        filename = 'calibration'
+        with open(f'svg/{filename}.path', 'r') as f:
+            path = f.read()
+            
+        xs = []
+        ys = []
+            
+        path = iter(path.split())
+        for action in path:
+            x = float(next(path)) * 0.8
+            y = float(next(path)) * 0.8
+            
+            xs.append(x)
+            ys.append(y)
+            
+            if action == 'M':
+                all_commands.extend(p.move_to(x, y))
+            elif action == 'L':
+                all_commands.extend(p.line_to(x, y))
+            else:
+                raise NotImplementedError()
     
-    # all_commands = []
-    # # all_commands.extend(p.line_to(220, 350))
-    # all_commands.extend(p.move_to(min(xs), min(ys)))
-    # all_commands.extend(p.line_to(max(xs), max(ys)))
-        
+    else:
+        all_commands = []
+        all_commands.extend(p.line_to(200, 200))
+        all_commands.extend(p.line_to(20, 200))
+      
     print(len(all_commands))
     send_commands(all_commands)
-    
+
     sim.draw_svg()
