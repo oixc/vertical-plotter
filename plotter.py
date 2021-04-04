2b409d0ac3a3aa0a6c64ec25da9bedd8beb2de14
oixc <jan.h.hofmann@googlemail.com>
2021-04-04 17:15:37 +0200

    more complicated sequence including pin up and down

--- plotter.py
+++ plotter.py
@@ -8,14 +8,14 @@ Created on Sun Apr  4 16:16:44 2021
 import arduino_serial
 import simulation
 
-# send_commands = arduino_serial.send_commands
+send_commands = arduino_serial.send_commands
 
 sim = simulation.Simulation()
-send_commands = sim.send_commands
+# send_commands = sim.send_commands
 
 # def send_commands(commands):
 #     sim.send_commands(commands)
 #     arduino_serial.send_commands(commands)
 
-send_commands(['c', 'c', 'd'] * 100)
-send_commands(['c', 'd', 'd'] * 100)
+send_commands(['f'] + ['c', 'c', 'a'] * 50 + ['e'] + ['a', 'a', 'd'] * 50 + ['f'] + ['a', 'c'] * 100 + ['e']) 
+# send_commands(['e'] + ['c', 'd', 'd'] * 100)
