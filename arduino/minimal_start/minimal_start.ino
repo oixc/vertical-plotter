820e6a1a2221d75747f708e52a9789d2c4fc23a7
oixc <jan.h.hofmann@googlemail.com>
2021-04-04 12:17:48 +0200

    alternate blinking two LEDs

--- arduino/minimal_start/minimal_start.ino
+++ arduino/minimal_start/minimal_start.ino
@@ -23,18 +23,22 @@
 */
 
 //const int ledPin =  LED_BUILTIN;  // the number of the LED pin
-const int ledPin =  12;  // the number of the LED pin
+const int ledPin1 =  11;
+const int ledPin2 =  12;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(ledPin, HIGH);        // turn the LED on (HIGH is the voltage level)
+  digitalWrite(ledPin1, LOW);        // turn the LED on (HIGH is the voltage level)
+  digitalWrite(ledPin2, HIGH);        // turn the LED on (HIGH is the voltage level)
   delay(2000);                       // wait for a second
-  digitalWrite(ledPin, LOW);         // turn the LED off by making the voltage LOW
+  digitalWrite(ledPin1, HIGH);         // turn the LED off by making the voltage LOW
+  digitalWrite(ledPin2, LOW);         // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
