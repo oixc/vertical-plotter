a9067d7dae05607d8fdfd1439ce3f146dbc89bf7
oixc <jan.h.hofmann@googlemail.com>
2021-04-04 22:25:35 +0200

    parse svg files into line_to and move_to paths

---
+++ svg_parser.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  4 21:20:39 2021
+
+@author: oixc
+"""
+
+from svgpathtools import svg2paths
+
+filename = 'calibration'
+paths, attributes = svg2paths(f'svg/{filename}.svg')
+
+with open(f'svg/{filename}.path', 'w') as f:
+    for k, v in enumerate(attributes):
+        print(v['d'])  # print d-string of k-th path in SVG
+        f.write(v['d'])  # print d-string of k-th path in SVG
+        
