ae99ecc6eb2423acb6779a8d3b4884c7fb6deff1
oixc <jan.h.hofmann@googlemail.com>
2021-04-05 12:45:08 +0200

    clean up parsed svg by replacing horizontal and vertical lines (H and V) with L

--- svg_parser.py
+++ svg_parser.py
@@ -15,3 +15,40 @@ with open(f'svg/{filename}.path', 'w') as f:
         print(v['d'])  # print d-string of k-th path in SVG
         f.write(v['d'])  # print d-string of k-th path in SVG
         
+
+clean_up = True
+if clean_up:
+    with open(f'svg/{filename}.path', 'r') as f:
+        path = f.read()
+    
+    clean_path = []
+    xs = []
+    ys = []
+        
+    path = path.replace(',', ' ')
+    path = iter(path.split())
+    for action in path:
+        if action in ['M', 'L']:
+            x = float(next(path))
+            y = float(next(path))
+        elif action == 'H':
+            x = float(next(path))
+            action = 'L'
+        elif action == 'V':
+            y = float(next(path))
+            action = 'L'
+        else:
+            raise NotImplementedError()
+            
+        clean_path.append(action)
+        clean_path.append(str(x))
+        clean_path.append(str(y))
+    
+        xs.append(x)
+        ys.append(y)
+        
+    clean_path = ' '.join(clean_path)
+    
+    with open(f'svg/{filename}.path', 'w') as f:
+        f.write(clean_path)
+        
