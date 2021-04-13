1dd855a269ec1fa37d8339f50ee95ad94830d972
oixc <jan.h.hofmann@googlemail.com>
2021-04-13 22:26:28 +0200

    tested stepper implementation

--- arduino/real_stepper/real_stepper.ino
+++ arduino/real_stepper/real_stepper.ino
@@ -40,102 +40,51 @@ void setup() {
   Serial.flush();
 }
 
-void blink_all() { 
-  digitalWrite(pins[MOTOR_LEFT][DIRECTION_LEFT], HIGH);    
-  digitalWrite(pins[MOTOR_LEFT][DIRECTION_RIGHT], HIGH);   
-  digitalWrite(pins[MOTOR_RIGHT][DIRECTION_LEFT], HIGH);    
-  digitalWrite(pins[MOTOR_RIGHT][DIRECTION_RIGHT], HIGH);   
-  delay(200);                 
-  digitalWrite(pins[MOTOR_LEFT][DIRECTION_LEFT], LOW);    
-  digitalWrite(pins[MOTOR_LEFT][DIRECTION_RIGHT], LOW);   
-  digitalWrite(pins[MOTOR_RIGHT][DIRECTION_LEFT], LOW);    
-  digitalWrite(pins[MOTOR_RIGHT][DIRECTION_RIGHT], LOW);   
-  delay(200);                 
-}
-
 void blink(int pin) { 
   digitalWrite(pin, HIGH);    
   delay(5);                 
   digitalWrite(pin, LOW);     
 }
   
-void rotate(int motor, int dir) { 
-  blink(pins[motor][dir]);
-//  switch (motor) {
-//    case MOTOR_LEFT:
-//      Serial.print('L');
-//      break;
-//    case MOTOR_RIGHT:
-//      Serial.print('R');
-//      break;
-//  }
-//  switch (dir) {
-//    case DIRECTION_LEFT:
-//      Serial.print('l');
-//      break;
-//    case DIRECTION_RIGHT:
-//      Serial.print('r');
-//      break;
-//  }
-//  Serial.println(); 
+void rotate(int motor, int dir, int steps) {
+  if (dir == DIRECTION_RIGHT) {
+    // Set the spinning direction clockwise:
+    digitalWrite(pins[motor][DIRECTION_PIN], HIGH);
+  } else {
+    // Set the spinning direction counter clockwise:
+    digitalWrite(pins[motor][DIRECTION_PIN], LOW);
+  }
+  for (int i = 0; i < steps; i++) {
+    // These four lines result in 1 step:
+    digitalWrite(pins[motor][STEP_PIN], HIGH);
+    delayMicroseconds(500);
+    digitalWrite(pins[motor][STEP_PIN], LOW);
+    delayMicroseconds(500);
+  }
 }
 
 void penDown() {         
   delay(500);        
-  digitalWrite(penPin, HIGH);              
-  delay(500);
+  // digitalWrite(penPin, HIGH);              
+  // delay(500);
 }
 
 void penUp() {         
   delay(500);        
-  digitalWrite(penPin, LOW);              
-  delay(500);
+  // digitalWrite(penPin, LOW);              
+  // delay(500);
 }
 
 char c;
-// String s;
 
 void loop() {
-//  blink(pins[MOTOR_LEFT][DIRECTION_LEFT]);
-//  blink(pins[MOTOR_LEFT][DIRECTION_RIGHT]);
-//  blink(pins[MOTOR_RIGHT][DIRECTION_LEFT]);
-//  blink(pins[MOTOR_RIGHT][DIRECTION_RIGHT]);
-//  delay(2000);                       
-
-//  rotate(MOTOR_LEFT, DIRECTION_LEFT);
-//  rotate(MOTOR_LEFT, DIRECTION_RIGHT);    
-//  rotate(MOTOR_RIGHT, DIRECTION_LEFT);
-//  rotate(MOTOR_RIGHT, DIRECTION_RIGHT);    
-//  delay(1000);     
-
   if (Serial.available() > 0) {
-//    blink_all();
-//    s = Serial.readString();
-//    s.trim();
-//    s.toLowerCase();
-//    if (s == "ll") {
-//      rotate(MOTOR_LEFT, DIRECTION_LEFT);
-//    }
-//    if (s == "lr") {
-//      rotate(MOTOR_LEFT, DIRECTION_RIGHT);
-//    }
-//    if (s == "rl") {
-//      rotate(MOTOR_RIGHT, DIRECTION_LEFT);
-//    }
-//    if (s == "rr") {
-//      rotate(MOTOR_RIGHT, DIRECTION_RIGHT);
-//    }
-//    //delayMicroseconds(300); 
-//    Serial.print(s);
-//    Serial.flush();
-
-//    blink_all();
     c = Serial.read();
     switch (c) {
-      case 'a': rotate(MOTOR_LEFT, DIRECTION_LEFT); break;
-      case 'b': rotate(MOTOR_LEFT, DIRECTION_RIGHT); break;
-      case 'c': rotate(MOTOR_RIGHT, DIRECTION_LEFT); break;
-      case 'd': rotate(MOTOR_RIGHT, DIRECTION_RIGHT); break;
+      case 'a': rotate(MOTOR_LEFT, DIRECTION_LEFT, 1); break;
+      case 'b': rotate(MOTOR_LEFT, DIRECTION_RIGHT, 1); break;
+      case 'c': rotate(MOTOR_RIGHT, DIRECTION_LEFT, 1); break;
+      case 'd': rotate(MOTOR_RIGHT, DIRECTION_RIGHT, 1); break;
       case 'e': penUp(); break;
       case 'f': penDown(); break;
     }
