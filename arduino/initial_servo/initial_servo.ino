896076777d250cd864c77955f2e08c6cef66027a
oixc <jan.h.hofmann@googlemail.com>
2021-04-14 21:49:18 +0200

    modify servo example to fit pen plotter testing

--- arduino/initial_servo/initial_servo.ino
+++ arduino/initial_servo/initial_servo.ino
@@ -9,23 +9,19 @@
 
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+#define SERVO_PIN 12
+#define SERVO_POS_UP 100
+#define SERVO_POS_DOWN 150 
 
-int pos = 0;    // variable to store the servo position
+Servo pen_servo;  // create servo object to control a servo
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pen_servo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+    pen_servo.write(SERVO_POS_UP);
+    delay(1000);                 
+    pen_servo.write(SERVO_POS_DOWN);
+    delay(1000);                
 }
