1857a2d78ba502f5581ac186b3aaec0eeb3c7559
oixc <jan.h.hofmann@googlemail.com>
2021-05-26 21:14:53 +0200

    gradually put down the pen to avoid jumping

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -15,9 +15,11 @@
 #define STEP_PIN 1
 
 #define PEN_PIN 12
-#define PEN_POS_UP 120
-#define PEN_POS_DOWN 80
-#define PEN_DELAY 100 
+#define PEN_POS_UP 100 // in degree
+#define PEN_POS_DOWN 80 // in degree
+#define PEN_DELAY 100 // in ms
+#define PEN_STEP_DELAY 10 // in ms
+#define PEN_STEP_SIZE 1 // in degree
 
 Servo pen_servo;  // create servo object to control the pen lift servo
 
@@ -27,6 +29,7 @@ int pins[2][2] = { // direction, step
 };
 
 int penPin = 4;
+int pos = 0;    // variable to store the servo position
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -73,7 +76,10 @@ void rotate(int motor, int dir, int steps) {
 
 void penDown() {         
   delay(PEN_DELAY);        
-  pen_servo.write(PEN_POS_DOWN);           
+  for (pos = PEN_POS_DOWN; pos <= PEN_POS_UP; pos += PEN_STEP_SIZE) {
+    pen_servo.write(pos);              
+    delay(PEN_STEP_DELAY );                       
+  }
   delay(PEN_DELAY);
 }
 
