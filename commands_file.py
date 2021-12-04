a663057700b5a0cad27f21f355ae04bedd58321b
oixc <jan.h.hofmann@googlemail.com>
2021-12-04 15:10:04 +0100

    commands file as external argument

--- commands_file.py
+++ commands_file.py
@@ -6,11 +6,18 @@ Created on Mon Nov  8 19:33:00 2021
 """
 
 import arduino_serial
+import argparse
 
 if __name__ == '__main__':
-    filename = 'plot_commands.txt'
-    with open(filename) as f:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', 
+                        default='plot_commands.txt',
+                        help='file to read commands from')
+    args = parser.parse_args()
+
+    with open(args.file) as f:
         commands = f.readlines()
     
-    print(len(commands[0]))
-    arduino_serial.send_commands(commands[0])
+    print(len(commands[0]) + 1)
+    arduino_serial.send_commands('e' + commands[0])
+    
